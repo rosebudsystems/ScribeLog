3431eb9950635230590798e6e05782615ac1922f
rosebudsystems <ale.javier.romero@gmail.com>
2025-01-15 23:18:37 -0300

    Create __init__.py
    Create __init__.py

---
+++ ScribeLog/__init__.py
@@ -0,0 +1,15 @@
+# __init__.py
+from .logger_module import FileLogger
+
+# Crear una instancia global
+_FileLogger_instance = FileLogger()
+
+# Exponer los métodos con tipado explícito
+def ConfigLog(pathLogin: str):
+    return _FileLogger_instance.ConfigLog(pathLogin)
+
+def GetPathLogin():
+    return _FileLogger_instance.GetPathLogin()
+
+def Log(message,time=True):
+    return _FileLogger_instance.Log(message,time)
